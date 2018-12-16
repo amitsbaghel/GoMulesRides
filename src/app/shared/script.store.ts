3a3bd3e8307da053f801f0774221e61b1377ed13
Amit Baghel <amitsb3747@gmail.com>
2018-12-16 00:29:47 -0600

    Update script.store.ts

--- src/app/shared/script.store.ts
+++ src/app/shared/script.store.ts
@@ -3,7 +3,7 @@ interface Scripts {
     src: string;
 }  
 export const ScriptStore: Scripts[] = [
-    {name: 'googledistance', src: 'https://maps.googleapis.com/maps/api/distancematrix/json?key=AIzaSyBe8qhHfBKRdeOK0iWAQiYtQWStEyQOXC4'},
-    {name: 'googlemaps', src: 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBe8qhHfBKRdeOK0iWAQiYtQWStEyQOXC4&libraries=places'}
+    {name: 'googledistance', src: 'https://maps.googleapis.com/maps/api/distancematrix/json?key=YOUR_API_KEY_HERE'},
+    {name: 'googlemaps', src: 'https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY_HERE&libraries=places'}
 
 ];
